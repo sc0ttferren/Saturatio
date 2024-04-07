b87ff759e18f74bc165f75be74b2c586b70f12a2
SC0TT <100329425+sc0ttferren@users.noreply.github.com>
2024-04-06 20:03:56 -0500

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,6 @@
 local e=Instance.new('BindableFunction')
-e.OnInvoke=function() setclipboard('https://discord.gg/G29CmH2RnS') end
-game.StarterGui:SetCore('SendNotification',{Title='Saturatio',Text='Thanks for using!',Icon='http://www.roblox.com/asset/?id=8941506186',Duration=5,Button1='discord.gg/XXqzxT7E5z',Callback=e})
+e.OnInvoke=function() setclipboard('https://discord.gg/Gsd6JdfhST') end
+game.StarterGui:SetCore('SendNotification',{Title='Saturatio',Text='Thanks for using!',Icon='http://www.roblox.com/asset/?id=8941506186',Duration=5,Button1='discord.gg/Gsd6JdfhST',Callback=e})
 local a=''
 for i,v in ipairs({10972284553,10710676163,4522347649,5023687570,286090429,6708206173,3101667897}) do if game.PlaceId==v then a=game:HttpGet('https://raw.githubusercontent.com/sc0ttferren/Saturatio/main/'..tostring(game.PlaceId)..'/.lua') end end
 loadstring(game:HttpGet('https://raw.githubusercontent.com/sc0ttferren/Saturatio/Universal/.lua')..a)()
